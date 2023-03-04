b225eaf650a73e85e713ec8b2624ef6f6fdd40ac
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-03-04 19:42:34 +0200

    styles for TransactionHistory

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,8 +2,12 @@ import css from './FriendListItem.module.css';
 
 export default function FriendListItem({ avatar, name, isOnline }) {
     return <li className={css.item}>
-        <span className={css.status} style={{backgroundColor: isOnline ? "var(--green)": "var(--tomato)"}}></span>
+        <span className={css.status} style={{backgroundColor: defineStatusColor(isOnline)}}></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
         <p className={css.name}>{name}</p>
     </li>
+}
+
+function defineStatusColor(isOnline) {
+    return isOnline ? "var(--green)" : "var(--tomato)";
 }
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,20 +1,21 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
-    return <table className="transaction-history">
+    return <table className={css["transaction-history"]}>
   <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+    <tr className={css.head}>
+      <th className={css["head-item"]}>Type</th>
+      <th className={css["head-item"]}>Amount</th>
+      <th className={css["head-item"]}>Currency</th>
     </tr>
   </thead>
 
-  <tbody>
-    {items.map(({id,type,amount,currency}) => <tr key={id}>
-        <td>{ type}</td>
-      <td>{ amount}</td>
-        <td>{currency }</td>
+  <tbody className={css["table-body"]}>
+    {items.map(({id,type,amount,currency}) => <tr className={css.row} key={id}>
+        <td className={[css["row-item"], css.type].join(' ') }>{ type}</td>
+      <td className={css["row-item"]}>{ amount}</td>
+        <td className={css["row-item"]}>{currency }</td>
     </tr>)}
   </tbody>
 </table>
